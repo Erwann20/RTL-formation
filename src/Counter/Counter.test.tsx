09f22f856f3b180ddf2c0eb53400a9d677167fd1
Bulle Ouvrard <bulle.ouvrard@gmail.com>
2024-02-20 15:18:28 +0100

    feat(dev-2796) - add unit tests

--- src/Counter/Counter.test.tsx
+++ src/Counter/Counter.test.tsx
@@ -3,14 +3,39 @@ import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 import { Counter } from './Counter'
 
-
 describe('Unit::Counter', () => {
     beforeEach(() => render(<Counter />))
-    it("should increment counter", async () => {
-        
-        const addElement = screen.getByText('+')
-        
-        await userEvent.click(addElement)
-        expect(screen.getByTestId('counter').textContent).toBe('1')
+
+    it('should render counter initial value as 0', () => {
+        const counter = screen.getByTestId('counter')
+        expect(counter.textContent).toBe('0')
+    })
+
+    it("should increment counter when + button clicked", async () => {
+        const incrementButton = screen.getByText('+')
+        const counter = screen.getByTestId('counter')
+        await userEvent.click(incrementButton)
+        expect(counter.textContent).toBe('1')
+    })
+
+    it("should decrement counter when - button clicked", async () => {
+        const decrementButton = screen.getByText('-')
+        const counter = screen.getByTestId('counter')
+        await userEvent.click(decrementButton)
+        expect(counter.textContent).toBe('0')
+    })
+
+    it("should set counter to 0 when reset button clicked", async () => {      
+        const resetButton = screen.getByText('reset')
+        const counter = screen.getByTestId('counter')
+        await userEvent.click(resetButton)
+        expect(counter.textContent).toBe('0')
+    })
+
+    it("should not decrement the counter below zero", async () => {
+        const decrementButton = screen.getByText('-')
+        const counter = screen.getByTestId('counter')
+        await userEvent.click(decrementButton)
+        expect(counter.textContent).toBe('0')
     })
 })
--- src/Counter/Counter.util.test.ts
+++ src/Counter/Counter.util.test.ts
@@ -1,7 +1,10 @@
-// import {formatNumberToString} from './Counter.util'
+import {formatNumberToString} from './Counter.util'
 
 describe('Unit::Counter.util', () => {
     describe('formatNumberToString', () => {
-     
+        it('should return a string', () => {
+            const result = formatNumberToString(0)
+            expect(result).toBe('0')
+        })
     })
 })
