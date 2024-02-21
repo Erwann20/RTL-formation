eb65d59e66ba7de8282294e44a62c7124c4935f8
Bulle Ouvrard <bulle.ouvrard@gmail.com>
2024-02-21 13:31:36 +0100

    feat(dev-2796) - fix formatNumberToString tests

--- src/Counter/Counter.util.test.ts
+++ src/Counter/Counter.util.test.ts
@@ -2,14 +2,19 @@ import {formatNumberToString} from './Counter.util'
 
 describe('Unit::Counter.util', () => {
     describe('formatNumberToString', () => {
-        it('should return a string', () => {
-            const result1 = formatNumberToString(0)
-            const result2 = formatNumberToString(1)
-            const result3 = formatNumberToString(-1)
+        it('should return 0 as a string', () => {
+            const result = formatNumberToString(0)
+            expect(result).toBe('0')
+        })
 
-            expect(result1).toBe('0')
-            expect(result2).toBe('1')
-            expect(result3).toBe('-1')
+        it('should return 1 as a string', () => {
+            const result = formatNumberToString(1)
+            expect(result).toBe('1')
+        })
+
+        it('should return -1 as a string', () => {
+            const result = formatNumberToString(-1)
+            expect(result).toBe('-1')
         })
     })
 })
