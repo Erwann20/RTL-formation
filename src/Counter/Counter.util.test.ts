2106a96547b1f46e75e49a13bea485850f258c17
Hamid <codin.hamster@gmail.com>
2024-02-21 11:09:43 +0100

    feat(dev-2794) - update test fonction et coverage/ dans gitignore

--- src/Counter/Counter.util.test.ts
+++ src/Counter/Counter.util.test.ts
@@ -1,19 +1,21 @@
 import { formatNumberToString } from "./Counter.util";
 
 describe("Unit::Counter.util", () => {
-  it("formatNumberToString", () => {
-    const number = 123;
-    const expectResult = "123";
-    const result = formatNumberToString(number);
+  describe("formatNumberToString", () => {
+    it("should convert Number to String", () => {
+      const number = 123;
+      const expectResult = "123";
+      const result = formatNumberToString(number);
 
-    expect(result).toBe(expectResult);
-  });
+      expect(result).toBe(expectResult);
+    });
 
-  it("formatNegativeNumberToString", () => {
-    const number = -123;
-    const expectResult = "-123";
-    const result = formatNumberToString(number);
+    it("should convert negative Number to String", () => {
+      const number = -123;
+      const expectResult = "-123";
+      const result = formatNumberToString(number);
 
-    expect(result).toBe(expectResult);
+      expect(result).toBe(expectResult);
+    });
   });
 });
